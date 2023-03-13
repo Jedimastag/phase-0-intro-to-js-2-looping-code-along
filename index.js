3bf404fdb127d5bbf18e11234ee3e1fe471f1bfe
George Irwin <jedimastag0@gmail.com>
2023-03-12 22:04:41 -0400

    complete loop lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let thanks = []
+  for (let i = 0; i < names.length; i++) {
+    thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return thanks;
+}
+
+console.log ["Guadalupe", "Ollie", "Aki"];
+console.log(writeCards(names,));
+
+
+let count = 10;
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
